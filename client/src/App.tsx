a1a13d22155c8a0fa39e80b4828a8926b5783823
Frank Kang <holycow001@gmail.com>
2025-05-29 13:19:44 -0700

    added routes to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './modules/header.tsx';
+import { Index } from './pages/index.tsx';
 
 function App() {
  
 
   return (
-    <>
-      
-    </>
+    
+      <Routes>
+        <Route path="/" element={<Header />} >
+        <Route index element={<Index />} />
+        </Route>
+      </Routes>
+    
   )
 }
 
--- client/src/modules/Header.tsx
+++ client/src/modules/Header.tsx
@@ -1,5 +1,8 @@
+import { Outlet } from "react-router-dom";
+
 export function Header() {
   return (
+    <div>
     <header className="header flex justify-between items-center p-4 bg-blue-500 text-white">
       <nav>
         <ul>
@@ -8,6 +11,8 @@ export function Header() {
           <li><a href="/contact">Contact</a></li>
         </ul>
       </nav>
+      <Outlet />
     </header>
+    </div>
   );
 }
